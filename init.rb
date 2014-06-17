74ef41b51bbcd5344b53ff776d1ec4090d2af3b4
Kamil Franckiewicz <kamil@franckiewicz.com.pl>
2014-06-17 18:35:12 +0200

    version 0.1

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :redmine_cancel_button do
+  name 'Redmine Cancel Button plugin'
+  author 'Kamil Franckiewicz'
+  description 'Show cancel button during update issue section'
+  version '0.0.1'
+end
