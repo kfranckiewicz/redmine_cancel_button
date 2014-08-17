580dcc2f6bbb13f990da3bbd0b13333f7a537344
Kamil Franckiewicz <kamil@franckiewicz.com.pl>
2014-08-17 17:06:26 +0200

    changes in README.rdoc in init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,9 @@
 Redmine::Plugin.register :redmine_cancel_button do
-  name 'Redmine Cancel Button plugin'
+  name 'Redmine Cancel Button'
   author 'Kamil Franckiewicz'
   description 'Show cancel button during update issue section'
   version '0.0.1'
+  author_url 'http://facebook.com/kfranckiewicz1'
+  requires_redmine :version_or_higher => '2.3.4'
+  url 'http://git.zettabajt.pl/public'
 end
