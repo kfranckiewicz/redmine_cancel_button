d64acb715eb07fcdb024714474660d284904134c
Kamil Franckiewicz <kamil@franckiewicz.com.pl>
2015-11-27 22:16:36 +0100

    modified: author_url version number requires_redmine_version added: link to github

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :redmine_cancel_button do
   name 'Redmine Cancel Button'
   author 'Kamil Franckiewicz'
   description 'Show cancel button during update issue section'
-  version '0.0.1'
-  author_url 'http://facebook.com/kfranckiewicz1'
-  requires_redmine :version_or_higher => '2.3.4'
+  version '0.0.2'
+  author_url 'https://www.linkedin.com/in/kamilfranckiewicz'
+  requires_redmine :version_or_higher => '2.3'
   url 'http://git.zettabajt.pl/public'
 end
